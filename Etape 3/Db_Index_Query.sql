1a330252c57629b9104b6b34a2b9bd358897b848
Antoine <151762383+Antoinegse@users.noreply.github.com>
2026-05-19 01:55:14 +0200

    Delete Etape 3 directory

--- Etape 3/Db_Index_Query.sql
+++
@@ -1,8 +0,0 @@
-CREATE INDEX idx_artwork_title
-ON Artwork(title);
-
-CREATE INDEX idx_exhibition_dates
-ON Exhibition(start_date, end_date);
-
-CREATE INDEX idx_booking_workshop
-ON booking(Id_Workshop);
